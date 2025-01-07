2beb36b9ad54c5bfdb72dbacfecc405553663114
Liam Brophy <liambrophy.design@gmail.com>
2025-01-07 12:52:54 -0500

    completes lab

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000,100); 
+
+const square = num1 => num1 ** 2;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2
